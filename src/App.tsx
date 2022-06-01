3d8b25d2f6ed3666297c4f524e95dde73ba23277
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-01 18:37:44 +0300

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <nav>
+        <Link to={"/sign_in"}>Sign in</Link> |{" "}
+        <Link to={"/sign_up"}>Sign up</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./routes/sign_in";
+import SignUp from "./routes/sign_up";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path={"/sign_in"} element={<SignIn />} />
+        <Route path={"/sign_up"} element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
