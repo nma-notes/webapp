d7f015648061bfcdc162338c7c1245084a2f9996
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-03 20:22:49 +0300

    add sign up page

--- src/routes/sign_up.tsx
+++ src/routes/sign_up.tsx
@@ -1,3 +1,30 @@
+import Input from "../components/Input";
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
+import LinkButton from "../components/LinkButton";
+
 export default function SignUp() {
-  return <h2>Sign Up</h2>;
+  return (
+    <div
+      className={"flex content-center justify-center items-center min-h-screen"}
+    >
+      <form
+        className={
+          "border p-10 rounded-2xl flex flex-col justify-center items-center"
+        }
+      >
+        <h2 className={"m2 text-xl"}>Sign Up</h2>
+        <Input type={"email"} placeholder={"Email"} />
+        <Input type={"password"} placeholder={"Password"} />
+        <Input type={"password"} placeholder={"Password confirmation"} />
+        <div className={"flex justify-between w-full m-5 items-center"}>
+          <Button>Sign Up</Button>
+          <LinkButton to={"/sign_up"}>Sign In</LinkButton>
+        </div>
+        <Link to={"/"} className={"hover:underline m-2 text-sm"}>
+          Back to home
+        </Link>
+      </form>
+    </div>
+  );
 }
