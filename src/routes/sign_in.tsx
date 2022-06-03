c6688525c28d3c4b1ae36b43288f0ae3a5236a08
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-03 20:02:54 +0300

    add sign in page

--- src/routes/sign_in.tsx
+++ src/routes/sign_in.tsx
@@ -1,3 +1,36 @@
+import { Link } from "react-router-dom";
+
 export default function SignIn() {
-  return <h2>Sign In</h2>;
+  return (
+    <div
+      className={"flex content-center justify-center items-center min-h-screen"}
+    >
+      <form
+        className={
+          "border p-10 rounded-2xl flex flex-col justify-center items-center"
+        }
+      >
+        <h2 className={"m2 text-xl"}>Sign In</h2>
+        <input
+          type={"email"}
+          placeholder={"Email"}
+          className={"text-black text-center p-1 m-2 rounded-lg"}
+        />
+        <input
+          type={"password"}
+          placeholder={"Password"}
+          className={"text-black text-center p-1 m-2 rounded-lg"}
+        />
+        <Link to={"/"} className={"self-end hover:underline m-2 text-sm"}>
+          Forgot your password?
+        </Link>
+        <div className={"flex justify-between w-full m-5 items-center"}>
+          <button className={"rounded border p-1"}>Sign In</button>
+          <Link to={"/sign_up"} className={"rounded border p-1"}>
+            Sign Up
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
