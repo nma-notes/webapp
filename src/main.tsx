f9f855f5c2f53726219afb43b745b6ce33535d41
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-03 20:25:01 +0300

    add about page template

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./routes/sign_in";
 import SignUp from "./routes/sign_up";
+import About from "./routes/About";
+import Layout from "./components/Layout";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,6 +15,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path={"/sign_in"} element={<SignIn />} />
         <Route path={"/sign_up"} element={<SignUp />} />
+        <Route
+          path={"/about"}
+          element={
+            <Layout>
+              <About />
+            </Layout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
