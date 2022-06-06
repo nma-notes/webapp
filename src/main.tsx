50d146d5d674a57a4dcb1da78715a651591393b4
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-06 18:49:22 +0300

    use auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -8,31 +8,34 @@ import SignUp from "./routes/SignUp";
 import About from "./routes/About";
 import Layout from "./components/Layout";
 import Contacts from "./routes/Contacts";
+import { AuthProvider } from "./AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path={"/sign_in"} element={<SignIn />} />
-        <Route path={"/sign_up"} element={<SignUp />} />
-        <Route
-          path={"/about"}
-          element={
-            <Layout>
-              <About />
-            </Layout>
-          }
-        />
-        <Route
-          path={"/contacts"}
-          element={
-            <Layout>
-              <Contacts />
-            </Layout>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path={"/sign_in"} element={<SignIn />} />
+          <Route path={"/sign_up"} element={<SignUp />} />
+          <Route
+            path={"/about"}
+            element={
+              <Layout>
+                <About />
+              </Layout>
+            }
+          />
+          <Route
+            path={"/contacts"}
+            element={
+              <Layout>
+                <Contacts />
+              </Layout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
