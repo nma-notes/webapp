a6fb294a32d1cafffebc11078c41724e61c276a8
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-13 18:07:28 +0300

    add states for sign in form

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -2,8 +2,17 @@ import { Link } from "react-router-dom";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import LinkButton from "../components/LinkButton";
+import { FormEvent, useState } from "react";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <div
       className={"flex content-center justify-center items-center min-h-screen"}
@@ -12,15 +21,27 @@ export default function SignIn() {
         className={
           "border p-10 rounded-2xl flex flex-col justify-center items-center"
         }
+        onSubmit={handleSubmit}
       >
         <h2 className={"m2 text-xl"}>Sign In</h2>
-        <Input type={"email"} placeholder={"Email"} />
-        <Input type={"password"} placeholder={"Password"} />
+        <Input
+          type={"email"}
+          placeholder={"Email"}
+          value={email}
+          title={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input
+          type={"password"}
+          placeholder={"Password"}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
         <Link to={"/"} className={"self-end hover:underline m-2 text-sm"}>
           Forgot your password?
         </Link>
         <div className={"flex justify-between w-full m-5 items-center"}>
-          <Button>Sign In</Button>
+          <Button type={"submit"}>Sign In</Button>
           <LinkButton to={"/sign_up"}>Sign Up</LinkButton>
         </div>
         <Link to={"/"} className={"hover:underline m-2 text-sm"}>
